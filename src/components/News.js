d118b4de90c9f4b2d6e008a58aa136528d0c4a9f
Chintan Patel <patelchintan843@gmail.com>
2023-01-19 12:13:53 +0530

    v24 (fetching API)

--- src/components/News.js
+++ src/components/News.js
@@ -4,9 +4,21 @@ import NewsItem from './NewsItem'
 export class News extends Component {
     render() {
         return (
-            <div>
-                This is a news Component
-                <NewsItem />
+            <div className='container my-4'>
+                <h2>Top headlines</h2>
+                <div className="row">
+
+                    <div className="col-md-4">
+                        <NewsItem title="myTitle" description="myDesc" />
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItem title="myTitle" description="myDesc" />
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItem title="myTitle" description="myDesc" />
+                    </div>
+
+                </div>
             </div>
         )
     }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let { title, description, imageUrl } = this.props;
     return (
-      <div>
-        This is a news item
+      <div className='my-3'>
+        <div className="card" style={{ width: "18rem" }}>
+          <img src="https://cdn.mos.cms.futurecdn.net/c3bTdgetMPKi6PMM2CdtLU-1200-80.jpg" className='card-img-top' alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/newsdetail" className='btn btn-sm btn-danger'>Read More</a>
+          </div>
+        </div>
       </div>
     )
   }
