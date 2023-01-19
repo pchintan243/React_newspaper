5a04917c810fafe027de7068df07a84141e363f7
Chintan Patel <patelchintan843@gmail.com>
2023-01-19 15:59:56 +0530

    v29 (favicon, loading logic)

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner';
 
 export class News extends Component {
 
@@ -15,39 +16,49 @@ export class News extends Component {
 
     // It will run after render
     async componentDidMount() {
-        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=b856a7b7857d4cb4a80d83ac76be3e4b&page=1&pageSize=20";
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=b856a7b7857d4cb4a80d83ac76be3e4b&page=1&pageSize=${this.props.pageSize}`;
+        // Set the logic for loading
+        // Loading gif will occur up to page Load
+        this.setState({ loading: true });
         let data = await fetch(url);
         let parsedData = await data.json();
         console.log(parsedData);
-        this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults })
+        this.setState({
+            articles: parsedData.articles,
+            totalResults: parsedData.totalResults,
+            // loading false after all data loaded succesfully
+            loading: false
+        })
     }
 
     // Use for go previous page
     handlePrevClick = async () => {
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=b856a7b7857d4cb4a80d83ac76be3e4b&page=${this.state.page - 1}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=b856a7b7857d4cb4a80d83ac76be3e4b&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+        this.setState({ loading: true });
         let data = await fetch(url);
         let parsedData = await data.json();
         // Page will be decrement by 1 if you click on previous button
         this.setState({
             page: this.state.page - 1,
-            articles: parsedData.articles
+            articles: parsedData.articles,
+            loading: false
         })
     }
 
     // Use for go Next page
     // When total number of results will be finish at that time you can't able to click the next button
     handleNextClick = async () => {
-        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+        if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
 
-        }
-        else {
-            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=b856a7b7857d4cb4a80d83ac76be3e4b&page=${this.state.page + 1}`;
+            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=b856a7b7857d4cb4a80d83ac76be3e4b&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+            this.setState({ loading: true });
             let data = await fetch(url);
             let parsedData = await data.json();
             // Page will be increment by 1 if you click on next button
             this.setState({
                 page: this.state.page + 1,
-                articles: parsedData.articles
+                articles: parsedData.articles,
+                loading: false
             })
         }
     }
@@ -55,14 +66,15 @@ export class News extends Component {
     render() {
         return (
             <div className='container my-4'>
-                <h2>Top headlines</h2>
+                <h2 className='text-center'>Top headlines</h2>
+                {this.state.loading && <Spinner />}
                 <div className="row">
                     {/* map function is use for looping */}
                     {/* It is print the data and works like for loop */}
                     {/* It must take return value */}
                     {/* slice is use for how many characters you want to show in your website (use for structure style)*/}
-
-                    {this.state.articles.map((element) => {
+                    {/* loading is use for when you click on button at that time first loading will start to execure after that content will be display */}
+                    {!this.state.loading && this.state.articles.map((element) => {
                         return <div className="col-md-4" key={element.url} >
                             {/* Pass the title, description, images, newsurl from the our json file which is store in element array. */}
                             {/* If title is null at that time empty string will be execute */}
@@ -76,7 +88,7 @@ export class News extends Component {
                 <div className="container d-flex justify-content-between">
                     {/* &larr is use for <- left side arrow or &rarr is use for -> right side arrow */}
                     <button disabled={this.state.page <= 1} type="button" className="btn btn-sm btn-danger" onClick={this.handlePrevClick}> &larr; Previous</button>
-                    <button type="button" className="btn btn-sm btn-danger" onClick={this.handleNextClick}>Next &rarr;</button>
+                    <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-sm btn-danger" onClick={this.handleNextClick}>Next &rarr;</button>
                 </div>
             </div>
         )
