33371897bd7807d53687d6aa6d7a950e6aced168
Chintan Patel <patelchintan843@gmail.com>
2023-01-19 18:02:54 +0530

    v31 (add: router page (without loading))

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 export default class App extends Component {
 
   render() {
     return (
       <div>
-        <Navbar />
-        <News pageSize={6} country="in" category="science" />
+        <Router>
+          <Navbar />
+          <Routes>
+            {/* If page is not loading when you click on the navbar content so use key to solve its */}
+            <Route exact path="/" element={<News key="general" pageSize={6} country="in" category="general" />} />
+            <Route exact path="/business" element={<News key="business" pageSize={6} country="in" category="business" />} />
+            <Route exact path="/entertainment" element={<News key="entertainment" pageSize={6} country="in" category="entertainment" />} />
+            <Route exact path="/general" element={<News key="general" pageSize={6} country="in" category="general" />} />
+            <Route exact path="/health" element={<News key="health" pageSize={6} country="in" category="health" />} />
+            <Route exact path="/science" element={<News key="science" pageSize={6} country="in" category="science" />} />
+            <Route exact path="/sport" element={<News key="sport" pageSize={6} country="in" category="sport" />} />
+            <Route exact path="/technology" element={<News key="technology" pageSize={6} country="in" category="technology" />} />
+          </Routes>
+        </Router>
       </div>
     )
   }
